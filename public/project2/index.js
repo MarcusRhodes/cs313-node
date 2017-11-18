cee858494412cffc741286ecacad64b564f6eb8e
marcusBYUI <rho15005@byui.edu>
2017-11-17 22:26:03 -0500

    test

--- public/project2/index.js
+++ public/project2/index.js
@@ -18,8 +18,12 @@ app.get('/getPerson', function(request, response) {
     getPerson(request, response);
 });
 
+app.get('/mail', function(request, response) {
+	calculateRate(request, response);
+});
+
 app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is running on port', app.get('port'));
 });
 
 function getPerson(request, response) {
@@ -78,5 +82,76 @@ function getPersonFromDb(id, callback) {
         });
     });
 
-} // end of getPersonFromDb*/
+}
+
+function calculateRate(request, response) {
+	var requestUrl = url.parse(request.url, true);
+
+	var mailType = requestUrl.query.operation;
+	var weight = Number(requestUrl.query.weight);
+	var result = 0;
+
+	if (mailType == "Stamped") {
+		if (weight < 1) {
+			result = 0.49;
+		}
+		else if (weight < 2) {
+			result = 0.70;
+		}
+		else if (weight < 3) {
+			result = 0.91;
+		}
+		else {
+			result = 1.12;
+		}
+	} else if (mailType == "Metered") {
+		if (weight < 1) {
+			result = 0.46;
+		}
+		else if (weight < 2) {
+			result = 0.67;
+		}
+		else if (weight < 3) {
+			result = 0.88;
+		}
+		else {
+			result = 1.09;
+		}		
+	} else if (mailType == "Flats") {
+		if (weight < 1) {
+			result = 0.98;
+		}
+		else if (weight < 2) {
+			result = 1.19;
+		}
+		else if (weight < 3) {
+			result = 1.40;
+		}
+		else {
+			result = 1.61;
+		}
+	} else if (mailType == "Parcel") {
+		if (weight < 1) {
+			result = 3.00;
+		}
+		else if (weight < 2) {
+			result = 3.00;
+		}
+		else if (weight < 3) {
+			result = 3.00;
+		}
+		else {
+			result = 3.00;
+		}
+	} else if (mailType == "Snail") {
+		result = "Well, if you are going by that, you will never get it there!";
+	} else {
+		
+	}
+
+	var params = {mailType: mailType, weight: weight, result: result}	
+	
+	response.render('pages/result', params);
+}
+
 
