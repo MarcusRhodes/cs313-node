a71231bc01d4f7cfdae30c12f3ccfe96787d69cb
marcusBYUI <rho15005@byui.edu>
2017-11-17 22:20:39 -0500

    test

--- public/project2/index.js
+++ public/project2/index.js
@@ -1,11 +1,10 @@
+//localhost:5000/getPerson?id=1
 
-
-//cd /home/marcus/node-js-getting-started/project2/
-//heroku local
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
-var url = require('url');
+
+var pg = require("pg"); // This is the postgres database connection module.
+const connectionString = 'postgres://postgres:aurelius6908@127.0.0.1:5432/familyhistory';//testUser, testPass
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,155 +14,69 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index')
-});
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
-app.get('/mail', function(request, response) {
-	calculateRate(request, response);
+app.get('/getPerson', function(request, response) {
+    getPerson(request, response);
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
 
-function calculateRate(request, response) {
-	var requestUrl = url.parse(request.url, true);
+function getPerson(request, response) {
+    // First get the person's id
+    var id = request.query.id;
 
-	var mailType = requestUrl.query.operation;
-	var weight = Number(requestUrl.query.weight);
-	var result = 0;
+   // TODO: It would be nice to check here for a valid id before continuing on...
 
-	if (mailType == "Stamped") {
-		if (weight < 1) {
-			result = 0.49;
-		}
-		else if (weight < 2) {
-			result = 0.70;
-		}
-		else if (weight < 3) {
-			result = 0.91;
-		}
-		else {
-			result = 1.12;
-		}
-	} else if (mailType == "Metered") {
-		if (weight < 1) {
-			result = 0.46;
-		}
-		else if (weight < 2) {
-			result = 0.67;
-		}
-		else if (weight < 3) {
-			result = 0.88;
-		}
-		else {
-			result = 1.09;
-		}		
-	} else if (mailType == "Flats") {
-		if (weight < 1) {
-			result = 0.98;
-		}
-		else if (weight < 2) {
-			result = 1.19;
-		}
-		else if (weight < 3) {
-			result = 1.40;
-		}
-		else {
-			result = 1.61;
-		}
-	} else if (mailType == "Parcel") {
-		if (weight < 1) {
-			result = 3.00;
-		}
-		else if (weight < 2) {
-			result = 3.00;
-		}
-		else if (weight < 3) {
-			result = 3.00;
-		}
-		else {
-			result = 3.00;
-		}
-	} else if (mailType == "Snail") {
-		result = "Well, if you are going by that, you will never get it there!";
-	} else {
-		
-	}
+   // use a helper function to query the DB, and provide a callback for when it's done
+    getPersonFromDb(id, function(error, result) {
+        // This is the callback function that will be called when the DB is done.
+        // The job here is just to send it back.
 
-	var params = {mailType: mailType, weight: weight, result: result}	
-	
-	response.render('pages/result', params);
+       // Make sure we got a row with the person, then prepare JSON to send back
+        if (error || result == null || result.length != 1) {
+            response.status(500).json({success: false, data: error});
+        } else {
+            var person = result[0];
+            response.status(200).json(result[0]);
+        }
+    });
 }
 
+function getPersonFromDb(id, callback) {
+    console.log("Getting person from DB with id: " + id);
 
-//cd /home/marcus/node-js-getting-started/
-//npm start
-//http://localhost:5000/form.html
-/*
-var express = require('express');
-var app = express();
-var url = require('url');
+   var client = new pg.Client(connectionString);
 
-app.set('port', (process.env.PORT || 5000));
+   client.connect(function(err) {
+        if (err) {
+            console.log("Error connecting to DB: ")
+            console.log(err);
+            callback(err, null);
+        }
 
-app.use(express.static(__dirname + '/public'));
+       var sql = "SELECT id, first, last, birthdate FROM person WHERE id = $1::int";
+        var params = [id];
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+       var query = client.query(sql, params, function(err, result) {
+            // we are now done getting the data from the DB, disconnect the client
+            client.end(function(err) {
+                if (err) throw err;
+            });
 
-app.get('/math', function(request, response) {
-	handleMath(request, response);
-});
+           if (err) {
+                console.log("Error in query: ")
+                console.log(err);
+                callback(err, null);
+            }
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+           console.log("Found result: " + JSON.stringify(result.rows));
 
-function handleMath(request, response) {
-	var requestUrl = url.parse(request.url, true);
+           // call whatever function the person that called us wanted, giving it
+            // the results that we have been compiling
+            callback(null, result.rows);
+        });
+    });
 
-	console.log("Query parameters: " + JSON.stringify(requestUrl.query));
+} // end of getPersonFromDb*/
 
-	// TODO: Here we should check to make sure we have all the correct parameters
-
-	var operation = requestUrl.query.operation;
-	var operand1 = Number(requestUrl.query.operand1);
-	var operand2 = Number(requestUrl.query.operand2);
-
-	computeOperation(response, operation, operand1, operand2);
-}
-
-function computeOperation(response, op, left, right) {
-	op = op.toLowerCase();
-
-	var result = 0;
-
-	if (op == "add") {
-		result = left + right;
-	} else if (op == "subtract") {
-		result = left - right;		
-	} else if (op == "multiply") {
-		result = left * right;
-	} else if (op == "divide") {
-		result = left / right;
-	} else {
-		// It would be best here to redirect to an "unknown operation"
-		// error page or something similar.
-	}
-
-	// Set up a JSON object of the values we want to pass along to the EJS result page
-	var params = {operation: op, left: left, right: right, result: result};
-
-	// Render the response, using the EJS page "result.ejs" in the pages directory
-	// Makes sure to pass it the parameters we need.
-	response.render('pages/result', params);
-
-}
-*/
