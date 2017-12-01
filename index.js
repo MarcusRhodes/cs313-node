4637f141abad01dd35b9b1fe4e8349d545a445b0
marcusBYUI <rho15005@byui.edu>
2017-12-01 17:52:52 -0500

    project2 - Almost done!

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-//http://localhost:5000/getFav?id=1
-//https://stormy-garden-79016.herokuapp.com/getFav?id=1
+//https://stormy-garden-79016.herokuapp.com/project/project.html
 
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+  extended: true
+}));  
 
 const querystring = require('querystring');
 
 var pg = require("pg"); //'postgres://postgres:aurelius6908@127.0.0.1:5432/fav'
-const connectionString = 'postgres://postgres:aurelius6908@127.0.0.1:5432/fav';
+const connectionString = process.env.DATABASE_URl;
 //process.env.DATABASE_URl;
 
 var url = require('url');
@@ -19,22 +24,22 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-//this gets the contents of the database
-app.get('/getFav', function(request, response) {
-    getFav(request, response);
-});
 //this is part of another assignment
 app.get('/mail', function(request, response) {
 	calculateRate(request, response);
-});
+});*/
 //adding the movie or game into postgres
-app.get('/add2DB', function(request, response) {
+app.post('/add2DB', function(request, response) {
 	add2DB(request, response);
+});
+app.get('/getList', function(request, response) {
+	getList(request, response);
 });
 //this is part of everything, though.
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+/*
 //all this is part of another assignment
 function calculateRate(request, response) {
 	var requestUrl = url.parse(request.url, true);
@@ -104,71 +109,10 @@ function calculateRate(request, response) {
 	var params = {mailType: mailType, weight: weight, result: result}	
 	
 	response.render('pages/result', params);
-}
+}*/
 
-function getFav(request, response) {
-   var id = request.query.id;
-   getPersonFromDb(id, function(error, result) {
-       if (error || result == null || result.length != 1) {
-           response.status(500).json({success: false, data: error});
-       } else {
-           var person = result[0];
-           response.status(200).json(result[0]);
-       }
-   });
-}
-
-function getPersonFromDb(id, callback) {
-   console.log("Getting person from DB with id: " + id);
-
-   var client = new pg.Client(connectionString);
-
-   client.connect(function(err) {
-        if (err) {
-            console.log("Error connecting to DB: ")
-            console.log(err);
-            callback(err, null);
-        }
-
-       var sql = "SELECT * FROM fav WHERE id = $1::int";
-       //var sql = "SELECT * FROM fav WHERE id = 0";
-        var params = [id];
-
-       var query = client.query(sql, params, function(err, result) {
-            client.end(function(err) {
-                if (err) throw err;
-            });
-
-           if (err) {
-                console.log("Error in query: ")
-                console.log(err);
-                callback(err, null);
-            }
-
-           console.log("Found result: " + JSON.stringify(result.rows));
-           callback(null, result.rows);
-        });
-    });
-
-} // end of getPersonFromDb*/
-
-function add2DB(request, response) {
-	var id = url.parse(request.url,true).query.title;
-   console.log(id);
-   var requestUrl = url.parse(request.url, true);
-   adding2DB(id, function(error, result) {
-       if (error || result == null || result.length != 1) {
-           response.status(500).json({success: false, data: error});
-       } else {
-           var person = result[0];
-           response.status(200).json(result[0]);
-           //response.status(200).json(id);
-       }
-   });//*/
-}
-
-function adding2DB(id, callback) {
-   var client = new pg.Client(connectionString);
+function getList(request, response) {
+	var client = new pg.Client(connectionString);
 
    client.connect(function(err) {
         if (err) {
@@ -180,19 +124,57 @@ function adding2DB(id, callback) {
        var sql = "SELECT * FROM fav";
 
        var query = client.query(sql, function(err, result) {
-       	client.end(function(err) {
-         	if (err) throw err;
-         });
-         if (err) {
-            console.log("Error in query: ")
+            client.end(function(err) {
+                if (err) throw err;
+            });
+
+           if (err) {
+                console.log("Error in query: ")
+                console.log(err);
+                callback(err, null);
+            }
+
+           //console.log("Found result: " + JSON.stringify(result.rows));
+           response.status(200).json(result.rows);
+           //callback(null, result.rows);
+        });
+    });
+    
+}
+
+function add2DB(request, response) {
+   var requestUrl = url.parse(request.url, true);
+   adding2DB(request.body, function(error, result) {
+       if (error || result == null || result.length != 1) {
+           response.status(500).json({success: false, data: error});
+       } else {
+           var person = result[0];
+           response.status(200).json(result[0]);
+       }
+   });//*/
+}
+
+function adding2DB(req, callback) {
+   var client = new pg.Client(connectionString);
+
+   client.connect(function(err) {
+        if (err) {
+            console.log("Error connecting to DB: ");
             console.log(err);
             callback(err, null);
-         }
-
-         console.log("Found result: " + JSON.stringify(result.rows));
-         callback(null, result.rows);
-    	 });
-    });
+        }
+  	var queryConfig = {
+  		text: 'INSERT INTO fav(username, favtype, imageurl, favname, note) VALUES($1, $2, $3, $4, $5);',
+  		values: [req.username, req.favtype, req.imageurl, req.favname, req.note]
+  	};
+  	console.log(queryConfig);
+   client.query(queryConfig, function (err, result) {
+     		if (err) {
+     			return console.error(err);
+     		}
+     		callback(null, result.rows);
+   	});
+   });
 
 }
 //cd /home/marcus/node-js-getting-started/
--- public/project/search.js
+++ public/project/search.js
@@ -17,10 +17,6 @@ function search() {
 	});
 }
 
-function add(title) {
-	console.log(title);
-}
-
 function updateList(data) {
 	if (data.Search && data.Search.length > 0) {
 		var resultList = $("#ulResults");
@@ -28,15 +24,74 @@ function updateList(data) {
 
 		var i = 0;
 		do {//name of image will change when I get it working
-			resultList.append('<p><img onclick="" src="' + data.Search[i].Poster + 
-				'" height="249" width="167" /><input type="text" name="image' + i + '" value="' + 
+			resultList.append('<p><img src="' + data.Search[i].Poster + 
+				'" height="249" width="167" /><input type="text" id="imageurl' + i + '" value="' + 
 				data.Search[i].Poster +	'" hidden></p>');
 			resultList.append('<p>' + data.Search[i].Title + 
-				'</p><input type="text" name="title" value="' +	data.Search[i].Title +	
+				'</p><input type="text" id="favname' + i + '" value="' +	data.Search[i].Title +	
 				'" hidden>');
-			resultList.append("<input type='submit' value='Add to list'><br>");
+			resultList.append('<input type="text" id="favtype' + i + '" value="' +	data.Search[i].Type +	
+				'" hidden>');
+			resultList.append('<p>Add a note! <input type="text" id="note' + i + '"></p>');
+			resultList.append("<button onclick='selection(" + i + ");'>Add to list</button><br>");
+			resultList.append("<div id='status" + i + "'></div>");
 			i++;
 		} while (i <= data.totalResults)
 	}
+
+}
+
+function selection(num) {
+	var imageurl = $("#imageurl" + num).val();
+	var favname = $("#favname" + num).val();
+	var username = $("#username").val();
+	var favtype = $("#favtype" + num).val();
+	var note = $("#note" + num).val();
+	
+	var params = {
+		imageurl: imageurl,
+		favname: favname,
+		username: username,
+		favtype: favtype,
+		note: note
+	};
+
+	$.post("/add2DB", params, function(result) {
+		if (result && result.success) {
+			$("#status" + num).text("Successfully logged in.");
+		} else {
+			$("#status" + num).text("Error logging in.");
+		}
+	});
+}
+
+function loadList() {
+	$.get("/getList", function(data, status){
+		// For debugging purposes, make a note that we're back
+		console.log("This is the loadlist function");
+		console.log(status);
+		var resultList = $("#list");
+		resultList.empty();
+		//resultList.append(data[0].favname);
+    	makeList(data);
+	});
+}
+
+function makeList(data) {
+		var i = 0;
+		var resultList = $("#list");
+		resultList.empty();
+		
+		do {//name of image will change when I get it working
+			resultList.append("THIS IS WORKING!!!");
+			
+			resultList.append('<p>Name: ' + data[i].username + '</p>');
+			resultList.append('<p>'+ data[i].username +'\'s favorite ' + data[i].favtype + ' is '+ data[i].favname +'. <br>');
+	
+			resultList.append('<img src="' + data[i].imageurl +	'" height="249" width="167" /></p>');
+			resultList.append('<p>'+ data[i].username +'\'s comment: ' + data[i].note + '</p>');
+			resultList.append('<br>');
+			i++;
+		} while (i <= Object.keys(data).length)
 
 }
